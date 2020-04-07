357562451b6209927427d39fd311164871633b18
Gonzalo Lema <gonzalo.lema@mercadolibre.com>
2020-04-07 12:29:48 -0300

    Initial Commit

---
+++ teamsearchapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  teamsearchapp
+//
+//  Created by Gonzalo Lema on 4/7/20.
+//  Copyright © 2020 Gonzalo Lema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
